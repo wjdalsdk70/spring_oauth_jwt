bd5f62c0a044fc1e8258c8a70fdfd5dbb478fae3
이정민 <kyungheeyj45@ajou.ac.kr>
2024-03-27 22:59:05 +0900

    프로젝트 생성 및 의존성 추가

---
+++ src/main/java/com/example/spring_oauth_jwt/SpringOauthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_oauth_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_oauth_jwt/SpringOauthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_oauth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
